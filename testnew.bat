b871db62cae5ccc4761cbda874e55a2876980374
Maniraja91 <n.maniraja91@gmail.com>
2023-04-27 19:39:24 +0530

    update

--- testnew.bat
+++ testnew.bat
@@ -1,6 +1,13 @@
 @echo off
 set /p sentence="Enter the sentence you want to copy: "
-echo %sentence% | clipboard
+echo %sentence% | clip
 echo Sentence copied to clipboard.
 pause
 
+@echo off
+set /p Name= "What is your name: "
+set /p Age="What is your age: "
+set /p Passion="What is your passion"
+echo %Name% aged %Age% is passioned about %Passion% | clip
+echo Thanks for your time
+pause
